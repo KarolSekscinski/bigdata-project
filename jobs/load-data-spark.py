7e1d8dd37ef54db15f7fd708346c636cbaa7f89e
Karol Sekscinski <socer404@gmail.com>
2024-05-10 21:57:00 +0200

    first job touch

---
+++ jobs/load-data-spark.py
@@ -0,0 +1,5 @@
+import sparknlp
+spark = sparknlp.start()
+# https://sparknlp.org/docs/en/concepts#downloading-and-using-a-pretrained-pipeline
+# https://huggingface.co/facebook/bart-large-mnli
+# https://spark.apache.org/docs/latest/api/python/index.html
