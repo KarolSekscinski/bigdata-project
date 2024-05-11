e1aeedc20923fa984deaf408a21a6af73b0fdf25
Mikołaj Burdzy <m.burdzy@odo24.pl>
2024-05-11 12:17:57 +0200

    loading data from hive

--- jobs/load-data-spark.py
+++ jobs/load-data-spark.py
@@ -1,5 +1,23 @@
-import sparknlp
-spark = sparknlp.start()
-# https://sparknlp.org/docs/en/concepts#downloading-and-using-a-pretrained-pipeline
-# https://huggingface.co/facebook/bart-large-mnli
-# https://spark.apache.org/docs/latest/api/python/index.html
+import sys
+from pyspark.sql import SparkSession
+
+
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("Accessing Hive Data") \
+    .config("hive.metastore.uris", "thrift://hive-metastore:9083") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+# Set the database to 'testdb1'
+spark.sql("USE testdb1")
+
+# Query the 'who' table for the first 100 records and load data into a DataFrame
+df = spark.sql("SELECT * FROM who LIMIT 100")
+# Save DataFrame to Hive table
+table_name = "res_who"
+df.write.mode("overwrite").saveAsTable(table_name)
+
+# Perform further operations on the DataFrame as needed
+# Close the output file
